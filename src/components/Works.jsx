5b39eba758ddb7d39da50fb9b7b7fee3ff93e632
Abdul Khan <ahkn63@gmail.com>
2024-04-26 18:41:12 -0400

    Added Audio Files

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Tilt from "react-tilt"
+import { Tilt } from "react-tilt"
 import { motion } from "framer-motion"
 
 import { styles } from "../styles"
@@ -34,6 +34,19 @@ const ProjectCard = ({
           />
 
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            {/* Live Url */}
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+
+            {/* Github Url */}
             <div
               onClick={() => window.open(source_code_link, "_blank")}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
@@ -72,6 +85,7 @@ const Works = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} `}>My work</p>
+
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
